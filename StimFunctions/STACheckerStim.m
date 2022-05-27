22ce4f4029ba8197de8466b61306ac0b91581e2c
peter-hogg <peterhogg2006@gmail.com>
2022-05-27 11:35:53 -0700

    Update GUI

--- StimFunctions/STACheckerStim.m
+++ StimFunctions/STACheckerStim.m
@@ -1,14 +1,9 @@
-function STACheckerStim(win, winRect);
-    % Define black and white
-    white = WhiteIndex(win)
-    black = BlackIndex(win)
-    grey = white / 2
+function STACheckerStim(win, winRect)
     % Get the size of the on screen window
-    [screenXpixels, screenYpixels] = Screen('WindowSize', win)
+    [screenXpixels, screenYpixels] = Screen('WindowSize', win);
     
-
     % Get the centre coordinate of the window
-    [xCenter, yCenter] = RectCenter(winRect)
+    [xCenter, yCenter] = RectCenter(winRect);
     
     % Make a base Rect of 200 by 200 pixels
     dim = 50;
@@ -20,30 +15,23 @@ function STACheckerStim(win, winRect);
     
     % Calculate the number of squares and reshape the matrices of coordinates
     % into a vector
-    [s1, s2] = size(xPos)
-    numSquares = s1 * s2
+    [s1, s2] = size(xPos);
+    numSquares = s1 * s2;
     xPos = reshape(xPos, 1, numSquares);
     yPos = reshape(yPos, 1, numSquares);
     
     % Scale the grid spacing to the size of our squares and centre
     xPosPlot = xPos .* dim + screenXpixels * 0.50;
     yPosPlot = yPos .* dim + yCenter;
-    
 
     
-    % Set the colors of each of our squares
-    %multiColors = rand(3, numSquares);
-    %bwColors = repmat(eye(2), 3, 3);
-    %bwColors = bwColors(1:end-1, 1:end-1);
-    %bwColors = reshape(bwColors, 1, numSquares);
-    %bwColors = repmat(bwColors, 3, 1)
-    
-    
-    r=randi(2,numSquares,1)-1
+    % Randomly choose which squares are black and white
+    r=randi(2,numSquares,1)-1;
+
     % Make our rectangle coordinates
     randPixel = nan(3, 3);
-    for i = 1:numSquares;
-        randPixel(:, i) = r(i)*255, r(i)*255, r(i)*255;
+    for i = 1:numSquares
+        randPixel(:, i) = r(i)*255; r(i)*255; r(i)*255;
     end
 
 
